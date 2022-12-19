d27dd3b1206c9a7fc3af3036a50c844881859ce8
magatdarwin <magat.darwin@gmail.com>
2022-12-19 15:32:00 +0800

    Complete playRound()

--- script.js
+++ script.js
@@ -6,6 +6,21 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    }
+    // Winning scenarios
+    else if (playerSelection === 'rock' && computerSelection === 'scissors' ||
+        playerSelection === 'scissors' && computerSelection === 'paper' ||
+        playerSelection === 'paper' && computerSelection === 'rock'
+        ) {
+        return `You win! ${playerSelection} beats ${computerSelection}!`;
+    }
+    else {
+        return `You lose! ${computerSelection} beats ${playerSelection}!`;
+    }
 
 }
 

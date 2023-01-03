52a7132a34846f6eed33c12d003e129311f11767
magatdarwin <magat.darwin@gmail.com>
2023-01-03 11:59:29 +0800

    Remove old game function

--- script.js
+++ script.js
@@ -38,24 +38,6 @@ function playRound() {
     updateScore();
 }
 
-function game() {
-    alert("Welcome to Rock Paper Scissors! You are about to play 5 rounds against the computer. Let's get started!");
-
-    for (i = 1; i < 6; i++) {
-        computerSelection = getComputerChoice();
-        while (true) {
-            playerSelection = prompt(`[ROUND ${i}] Choose between rock, paper and scissors`).toLowerCase();
-
-            if (!CHOICES.includes(playerSelection)) {
-                alert('Invalid choice! Please choose between rock, paper and scissors only.');
-            }
-            else break;
-        }
-
-        console.log(playRound(playerSelection, computerSelection));
-    }
-}
-
 function capitalizeFirstLetter(word) {
     return word.charAt(0).toUpperCase() + word.slice(1);
 }
